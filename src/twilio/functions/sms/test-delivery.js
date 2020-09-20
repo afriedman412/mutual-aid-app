37949299ba095a0aa3250db0081dff600a774caf
afriedman412 <afriedman412@gmail.com>
2020-09-20 00:07:40 -0400

    unknown volunteers get redirected to the signup form

--- src/twilio/functions/sms/test-delivery.js
+++ src/twilio/functions/sms/test-delivery.js
@@ -6,12 +6,13 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
   });
 
   const base = Airtable.base("apppPfEXed7SRcKmB"); // staging base
-  let code = event.body.requestCode;
-  const deliveryPhone = event.body.phoneNumber;
-  let deliveryName = "";
-  let missinginfo = false;
-  let missingintake = false;
+  let code = event.Body;
+  const deliveryPhone = event.From;
+  const deliveryName = "MAB TEST"; // get real delivery vol name from request
   code = code.toUpperCase().trim();
+
+  // for this prototype, you text the code to a twilio number.
+  // in prod, the event will be a webhook from a front-end button
 
   console.log("incoming message received");
 
@@ -27,84 +28,65 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         console.log("Retrieved", record.get("Code"));
         console.log("ID", record.id);
         const phone = record.get("Phone");
-        let firstName = record.get("First Name");
-        if (typeof firstName === "undefined") {
-          firstName = "your neighbor";
-        }
+        const firstName = record.get("First Name");
         const list = record.get("Intake General Notes");
-        if (typeof list === "undefined") {
-          missinginfo = true;
-        }
         const street1 = record.get("Cross Street #1");
-        if (typeof street1 === "undefined") {
-          missinginfo = true;
-        }
         const street2 = record.get("Cross Street #2");
-        if (typeof street2 === "undefined") {
-          missinginfo = true;
-        }
         const volrecord = record.get("Intake volunteer");
 
-        // looks for delivery volunteer name based on phone number
-        base("Volunteers")
-          .select({
-            view: "Grid view", // from staging base
-            maxRecords: 1,
-            filterByFormula: `({volunteer_phone} = '${deliveryPhone}')`,
-          })
-          .firstPage(function deliveryNameSeek(derror, dnamerecords) {
-            dnamerecords.forEach(function getName(dnamerecord) {
-              console.log("Retrieved", dnamerecord.get("volunter_name"));
-              console.log("ID", dnamerecord.id);
-              deliveryName = dnamerecord.get("volunteer_name");
-              if (typeof deliveryName === "undefined") {
-                deliveryName = "a CHMA volunteer";
-              }
-            });
-          });
+        // below: changes request status to "Delivery Assigned"
+        base("Requests").update(
+          record.id,
+          {
+            Status: "Delivery Assigned",
+          },
+          function reAssign(reerror) {
+            if (reerror) {
+              console.error(reerror);
+            }
+          }
+        );
 
         // below, uses linked 'Intake volunteer' record id to get phone number and name from Volunteers table
         base("Volunteers").find(volrecord, function checkVolunteer(volerror, vrecord) { // eslint-disable-line
           if (volerror) {
-            console.error(volerror);
-            return;
+            // if no record is found, send a link to the sign-up for
+            console.log('phone number not found!')
+            const twilioClient = context.getTwilioClient();
+
+            twilioClient.messages
+              .create({
+                to: event.From, // need delivery volunteer phone number here
+                from: event.To, // will input twilio number here
+                body: `
+                  <Response>
+                    <Message>Thanks for volunteering for this delivery but we don't have you in our records! Please <![sign up[https://airtable.com/shraIzCNdiMwW1bpP]]> to continue.</Message>
+                  </Respones>
+                  `,
+              })
+
+              return;
+
           }
+
           console.log("Retrieved", vrecord.id);
           const intakephone = vrecord.get("volunteer_phone");
-          let intakename = vrecord.get("volunteer_name");
-
-          if (typeof intakephone === "undefined") {
-            missingintake = true;
-          }
-
-          if (typeof intakename === "undefined") {
-            intakename = "ready to help";
-          }
-
-          let deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes well!`;
-
-          if (missinginfo === true) {
-            deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nIt looks like some important info might be missing - please follow up with #wg_tech on Slack, or text your intake volunteer ${intakename} at ${intakephone} - we'll get it sorted out!`;
-          }
-
-          if (missingintake === true) {
-            deliveryText = `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nIt looks like some important info might be missing - please follow up with #intake_volunteers on Slack - we'll get it sorted out`;
-          }
+          const intakename = vrecord.get("volunteer_name");
 
           // sends SMS out to delivery volunteer
           const twilioClient = context.getTwilioClient();
 
           twilioClient.messages
             .create({
-              to: deliveryPhone,
-              from: "+19175403381", // twilio number here
-              body: deliveryText,
+              to: event.From, // need delivery volunteer phone number here
+              from: event.To, // will input twilio number here
+              body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes well!`,
             })
             .then(function txtIntake() {
               twilioClient.messages.create(
                 {
                   to: intakephone,
-                  from: "+19175403381", // twilio number here
+                  from: event.To, // will input twilio from number here
                   body: `hey! The request with code ${code} for ${firstName} has been picked up by ${deliveryName}! please remember to check in with them, and make sure the request gets completed - their phone # is ${deliveryPhone}. thnx!`,
                 },
                 function finish() {
